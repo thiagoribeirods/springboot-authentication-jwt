78572d1a2e434ee8ed92f424921b96b9ce9cf314
Thiago <thiagoribeirods@outlook.com>
2023-06-11 13:43:41 -0300

    adding packages configuration

--- src/main/java/authentication/authentication/AuthenticationApplication.java
+++ src/main/java/authentication/authentication/AuthenticationApplication.java
@@ -2,12 +2,27 @@ package authentication.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class AuthenticationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationApplication.class, args);
-	}
+@ComponentScan(basePackages = {
+    "authentication.configuration",
+    "authentication.model",
+    "authentication.repository",
+    "authentication.service"
+})
+
+@EntityScan(basePackages = {"authentication.model"})
+@EnableJpaRepositories(basePackages = {"authentication.repository"})
+public class AuthenticationApplication 
+{
+
+    public static void main(String[] args) 
+    {
+        SpringApplication.run(AuthenticationApplication.class, args);
+    }
 
 }
