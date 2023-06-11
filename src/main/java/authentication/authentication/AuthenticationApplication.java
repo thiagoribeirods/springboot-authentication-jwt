cfbc5d3727792742062fe147a219333562255ace
Thiago <thiagoribeirods@outlook.com>
2023-06-11 13:59:36 -0300

    solving javadoc alert

--- src/main/java/authentication/authentication/AuthenticationApplication.java
+++ src/main/java/authentication/authentication/AuthenticationApplication.java
@@ -6,23 +6,24 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+/**
+ * 
+ * @author thiago-ribeiro
+ */
 
+@SpringBootApplication
 @ComponentScan(basePackages = {
     "authentication.configuration",
     "authentication.model",
     "authentication.repository",
     "authentication.service"
 })
-
 @EntityScan(basePackages = {"authentication.model"})
 @EnableJpaRepositories(basePackages = {"authentication.repository"})
 public class AuthenticationApplication 
 {
-
     public static void main(String[] args) 
     {
         SpringApplication.run(AuthenticationApplication.class, args);
     }
-
 }
