0df78f27f8e8b1397c2ba1e6b5c73ad232f26d4e
Thiago <thiagoribeirods@outlook.com>
2023-06-11 14:12:25 -0300

    implemeting userDetails

--- src/main/java/authentication/model/User.java
+++ src/main/java/authentication/model/User.java
@@ -3,7 +3,12 @@ package authentication.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import java.util.Collection;
+import java.util.List;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  *
@@ -11,7 +16,7 @@ import lombok.Data;
  */
 @Entity(name = "TB_USER")
 @Data
-public class User 
+public class User implements UserDetails
 {
     @Id
     private String username;
@@ -19,4 +24,29 @@ public class User
     private String password;
     
     private String name;
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
